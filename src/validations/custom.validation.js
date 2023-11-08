e6a0e0f111db98305886069e3284e8efe132d669
Makai <tronghaibk2008@gmail.com>
2023-11-08 09:15:30 +0700

    Add custom.validation.js

---
+++ src/validations/custom.validation.js
@@ -0,0 +1,6 @@
+export const validateId = (value, helpers) => {
+  if (!value.match(/^[0-9a-fA-F]{24}$/)) {
+    return helpers.message('"{{#label}}" must be a valid mongo id');
+  }
+  return value
+};
