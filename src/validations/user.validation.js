0ef212196f6fdf19b4ec728a533846281c5e246d
Makai <tronghaibk2008@gmail.com>
2023-11-08 09:56:18 +0700

    Update user.controller.js

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,4 +1,19 @@
 import Joi from 'joi';
+import {validateId} from './custom.validation';
+
+export const getUsers = {
+  query: Joi.object().keys({
+    limit: Joi.number()
+      .integer()
+      .min(0)
+      .max(100)
+      .default(50),
+    offset: Joi.number()
+      .integer()
+      .min(0)
+      .default(0),
+  }),
+}
 
 export const createUser = {
   body: Joi.object().keys({
@@ -12,5 +27,30 @@ export const createUser = {
       .max(255)
       .required(),
     name: Joi.string(),
-  })
-}
+  }),
+};
+
+export const getUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(validateId),
+  }),
+};
+
+export const updateUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(validateId),
+  }),
+  body: Joi.object().keys({
+    name: Joi.string(),
+    password: Joi.string()
+      .min(6)
+      .max(255),
+    isActive: Joi.boolean(),
+  }),
+};
+
+export const deleteUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(validateId),
+  }),
+};
