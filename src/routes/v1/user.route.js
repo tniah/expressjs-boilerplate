2c408590376c7a59c9f829b59f677a7e9c3d16d8
Makai <tronghaibk2008@gmail.com>
2023-11-08 09:57:17 +0700

    Update user.route.js

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -1,8 +1,19 @@
 import {Router} from "express";
 import * as userController from '../../controllers/user.controller';
+import {userValidation} from '../../validations'
+import {validate} from '../../middlewares/validate';
 
 const router = Router();
 
-router.route('/').get(userController.getUsers)
+router
+  .route('/')
+  .get(validate(userValidation.getUsers), userController.getUsers)
+  .post(validate(userValidation.createUser), userController.createUser);
+
+router
+  .route('/:userId')
+  .get(validate(userValidation.getUser), userController.getUser)
+  .patch(validate(userValidation.updateUser), userController.updateUser)
+  .delete(validate(userValidation.deleteUser), userController.deleteUser);
 
 export default router;
